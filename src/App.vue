4e4c5f7f5f00f3b4f3c35d1983f2371e1daf824f
BartaBalazs00 <77434768+BartaBalazs00@users.noreply.github.com>
2022-02-08 14:30:10 +0100

    adatok betöltése

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <thead>
+        <tr>
+          <th>Azonsító</th>
+          <th>Person</th>
+          <th>Height</th>
+          <th>Price</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statues" v-bind:key="statue.id">
+          <td>{{ statue.id }}</td>
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+          <td>
+            <button >Törlés</button>
+            <button >Szerkesztés</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="hidden" v-model="statue.id">
+          </td>
+          <td>
+            <input type="text" v-model="statue.person">
+          </td>
+          <td>
+            <input type="number" v-model="statue.height">
+          </td>
+          <td>
+            <input type="number" v-model="statue.price">
+          </td>
+          <td>
+            <button>Létrehoz</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      mod_new: true, 
+      saving: false,
+      statue: {
+        id: null,
+        person: '',
+        height: '',
+        price: ''
+      },
+      statues: []
+    }
+  },
+  methods: {
+    async loadData () {
+     let Response = await fetch('http://127.0.0.1:8000/api/statues')
+     let data = await Response.json()
+     this.statues = data
+    }
+  },
+  mounted() {
+    this.loadData()
   }
 }
 </script>
